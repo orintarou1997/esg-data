1dec98cae28fb37de6e712452ffe10ff15335fd1
Kuma C <13heartache.kc@gmail.com>
2023-11-08 21:51:35 -0500

    first commit

---
+++ src/components/Barchart.jsx
@@ -0,0 +1,9 @@
+import * as d3 from 'd3';
+import { useEffect, useRef } from "react";
+
+const Barchart = () => {
+  // set the dimensions and margins of the graph
+  return <div>Hello</div>;
+};
+
+export default Barchart;
